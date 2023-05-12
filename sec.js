20214a25d932e7dacda312e1f8d823469af13a91
DKoroli <dimakoroliuk@gmail.com>
2023-05-12 06:40:18 +0300

    12.05.23

--- sec.js
+++ sec.js
@@ -1,30 +1,15 @@
-const serverForm = [
-  { text: "ФИО", type: "text" },
-  { text: "Адрес стройки", type: "text" },
-  { text: "Загрузите Ваш файл", type: "file" },
-];
+// const serverForm = [
+//   { text: "ФИО", type: "text" },
+//   { text: "Адрес стройки", type: "text" },
+//   { text: "Загрузите Ваш файл", type: "file" },
+// ];
 
-// const formTypes = {
-//   construction: {
-//     name: "ФИО",
-//     adress: "Адрес стройки",
-//     inputType: ["text", "file"],
-//   },
-//   complaint: {
-//     name: "ФИО",
-//     adress: "Адрес жалобы",
-//     inputType: ["text", "file"],
-//   },
-//   sign: {
-//     name: "ФИО",
-//     adress: "Адрес установки знака",
-//     inputType: ["text", "file"],
-//   },
-//   trade: {
-//     name: "ФИО",
-//     adress: "Адрес места торговли",
-//     inputType: ["text", "file"],
-//   },
+// const serverFormS = {
+//   construction: [
+//     { text: "ФИО", type: "text" },
+//     { text: "Адрес стройки", type: "text" },
+//     { text: "Загрузите Ваш файл", type: "file" },
+//   ],
 // };
 
 const select = document.getElementById("dep");
@@ -34,26 +19,33 @@ select.addEventListener("change", (e) => {
   if (subCutDiv) {
     subCutDiv.remove();
   }
+  let formData;
   if (e.target.value == "construction") {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "http://127.0.0.1:8080/construction", false);
     xhr.send();
     addDomForSubcat(JSON.parse(xhr.response));
+    formData = JSON.parse(xhr.response.form);
+    console.log(formData);
+    console.log(xmr.response);
   } else if (e.target.value == "complaint") {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "http://127.0.0.1:8080/complaint", false);
     xhr.send();
-    addDomForSubcat(JSON.parse(xhr.response));
+    addDomForSubcat(JSON.parse(xhr.response.options));
+    formData = JSON.parse(xhr.response.form);
   } else if (e.target.value == "sign") {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "http://127.0.0.1:8080/sign", false);
     xhr.send();
-    addDomForSubcat(JSON.parse(xhr.response));
+    addDomForSubcat(JSON.parse(xhr.response.options));
+    formData = JSON.parse(xhr.response.form);
   } else {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "http://127.0.0.1:8080/trade", false);
     xhr.send();
-    addDomForSubcat(JSON.parse(xhr.response));
+    addDomForSubcat(JSON.parse(xhr.response.options));
+    formData = JSON.parse(xhr.response.form);
   }
   const form_div = document.getElementById("form_div");
   if (form_div) {
@@ -65,7 +57,7 @@ select.addEventListener("change", (e) => {
     if (form_div) {
       form_div.remove();
     }
-    newFormDom();
+    newFormDom(formData);
   });
 });
 
@@ -87,79 +79,33 @@ function addDomForSubcat(arr) {
   const div = document.createElement("div");
   div.id = "select_div";
   const selector = addSelectForSubcat(arr);
-  // if (e.target.value == "construction") {
-  //   selector.id = 'subconstr'
-  // } else if (e.target.value == "complaint") {
-  //   selector.id = 'subcompl'
-  // } else if (e.target.value == "sign") {
-  //   selector.id = 'subsign'
-  // } else {
-  //   selector.id = 'subtrade'
-  // }
   div.appendChild(selector);
   fragment.appendChild(div);
   document.body.appendChild(fragment);
 }
 
-function addFormDom() {
+function newFormDom(arr) {
   const fragment = document.createDocumentFragment();
   const div = document.createElement("div");
   div.id = "form_div";
   const form = document.createElement("form");
-  const pName = addDomForForm("ФИО:", "text");
-  const pAdress = addDomForForm("Ваш адрес", "text");
-  const pFile = addDomForForm("Загрузите Ваш файл:", "file");
-  form.appendChild(pName.text);
-  form.appendChild(pName.input);
-  form.appendChild(pAdress.text);
-  form.appendChild(pAdress.input);
-  form.appendChild(pFile.text);
-  form.appendChild(pFile.input);
+  for (i = 0; i < arr.length; i++) {
+    const result = createFormRow(arr[i].text, arr[i].type);
+    form.appendChild(result.text);
+    form.appendChild(result.input);
+  }
   div.appendChild(form);
   fragment.appendChild(div);
   document.body.appendChild(fragment);
 }
 
-function addDomForForm(arg1, arg2) {
+function createFormRow(arg1, arg2) {
   const paragraf = document.createElement("p");
   const pNameText = document.createTextNode(arg1);
   paragraf.appendChild(pNameText);
   const input = document.createElement("input");
   input.type = arg2;
   return { text: paragraf, input: input };
-}
-
-// ---------new functions----------------
-function newFormDom() {
-  const fragment = document.createDocumentFragment();
-  const div = document.createElement("div");
-  div.id = "form_div";
-  const form = document.createElement("form");
-  const pName = newDomForForm(serverForm);
-  const pAdress = newDomForForm(serverForm);
-  const pFile = newDomForForm(serverForm);
-  form.appendChild(pName);
-  // form.appendChild(pName.input);
-  form.appendChild(pAdress);
-  // form.appendChild(pAdress.input);
-  form.appendChild(pFile);
-  // form.appendChild(pFile.input);
-  div.appendChild(form);
-  fragment.appendChild(div);
-  document.body.appendChild(fragment);
-}
-
-function newDomForForm(arg) {
-  for (i = 0; i < arg.length; i++) {
-    const paragraf = document.createElement("p");
-    const paragrafText = document.createTextNode(arg[i].text);
-    console.log(paragrafText);
-    paragraf.appendChild(paragrafText);
-    const input = document.createElement("input");
-    input.type = arg[i].type;
-    console.log(typeof input);
-  }
-  return [{ text: paragraf, input: input }];
 }
 
 // --- call to backend ---
@@ -173,114 +119,3 @@ console.log("xhr.body", xhr.body);
 console.log("xhr.getAllResponseHeaders()", xhr.getAllResponseHeaders());
 console.log("xhr.response", xhr.response);
 console.log("xhr", xhr);
-
-// -----------------варианты--------------------
-// ---------- 1 variant ---------
-// function addFormDom(obj) {
-//     const fragment = document.createDocumentFragment();
-//   const div = document.createElement("div");
-//   div.id = "form_div";
-//   const form = document.createElement("form");
-//   for (i = 0; < obj.inputsText; i++) {
-//       const pName = addDomForForm(obj.inputsText[i], obj.inputType[i]);
-//       form.appendChild(pName.text);
-//       form.appendChild(pName.input);
-//     }
-//     div.appendChild(form);
-//   fragment.appendChild(div);
-//   document.body.appendChild(fragment);
-// }
-
-// function addDomForForm(arg1, arg2) {
-//     const paragraf = document.createElement("p");
-//     const pNameText = document.createTextNode(arg1);
-//     paragraf.appendChild(pNameText);
-//     const input = document.createElement("input");
-//     input.type = arg2;
-//     return { text: paragraf, input: input };
-//   }
-
-//   const construction = {
-//         inputsText: ["ФИО", "Адрес стройки","Загрузите Ваш файл"],
-//         inputType: ["text", "text", "file"],
-//       }
-
-//   const complaint = {
-//     inputsText: ["ФИО", "Адрес жалобы","Загрузите Ваш файл"],
-//     inputType: ["text", "text", "file"],
-//   }
-
-//   const sign = {
-//     inputsText: ["ФИО", "Адрес жалобы","Загрузите Ваш файл"],
-//     inputType: ["text", "text", "file"],
-//   }
-
-// ---------- 2variant -------
-// function addFormDom(arr) {
-//   const fragment = document.createDocumentFragment();
-//   const div = document.createElement("div");
-//   div.id = "form_div";
-//   const form = document.createElement("form");
-//   for (i = 0; < arr; i++) {
-//     const pName = addDomForForm(arr[i].text, arr[i].type);
-//     form.appendChild(pName.text);
-//     form.appendChild(pName.input);
-//   }
-//   div.appendChild(form);
-//   fragment.appendChild(div);
-//   document.body.appendChild(fragment);
-// }
-
-// function addDomForForm(arg1, arg2) {
-//   const paragraf = document.createElement("p");
-//   const pNameText = document.createTextNode(arg1);
-//   paragraf.appendChild(pNameText);
-//   const input = document.createElement("input");
-//   input.type = arg2;
-//   return { text: paragraf, input: input };
-// }
-
-//     construction: [
-//       {text: "ФИО", type: "text"},
-//       {text: "Адрес стройки", type: "text"},
-//       {text: "Download thewr dfile", type: "file"}
-//   ],
-
-//     ------- snippet --------
-// select.addEventListener("change", (e) => {
-//   const subCutDiv = document.getElementById("select_div");
-//   if (subCutDiv) {
-//     subCutDiv.remove();
-//   }
-//   if (e.target.value == "construction") {
-//     const xhr = new XMLHttpRequest();
-//     xhr.open("GET", "http://127.0.0.1:8080/construction", false);
-//     xhr.send();
-//     arr = [
-//       "--Выберите раздел--",
-//       "строительство парковки",
-//       "строительство ларька",
-//       "строительство многоэтажки",
-//     ]
-//     serverForm = [
-//       {text: "ФИО", type: "text"},
-//       {text: "Адрес стройки", type: "text"},
-//       {text: "Download thewr dfile", type: "file"}
-//     ]
-//     pseudoResponse = {arr: arr, form: serverForm}
-//     addDomForSubcat(pseudoResponse.arr);
-//     const form_div = document.getElementById("form_div");
-//     if (form_div) {
-//       form_div.remove();
-//     }
-//     const addedSel = document.getElementById("addedSel");
-//     addedSel.addEventListener("change", (e) => {
-//       const form_div = document.getElementById("form_div");
-//       if (form_div) {
-//         form_div.remove();
-//       }
-//       addFormDom(pseudoResponse.serverForm);
-//     });
-
-//   } else if (e.target.value == "complaint") {
-// ------- snippet ---------
--- server.js
+++ server.js
@@ -1,55 +1,94 @@
-var http = require('http');
+var http = require("http");
 
 const applicationTypes = {
-    construction: [
-      "--Выберите раздел--",
-      "строительство парковки",
-      "строительство ларька",
-      "строительство многоэтажки",
-    ],
-    complaint: ["--Выберите раздел--", "жалоба на соседа", "жалоба на полицию"],
-    sign: ["--Выберите раздел--", "разрешение на установку знака"],
-    trade: ["--Выберите раздел--", "продажа пирожков", "продажа цветов", "продажа игрушек", "продажа продуктов"],
-  };
+  construction: [
+    "--Выберите раздел--",
+    "строительство парковки",
+    "строительство ларька",
+    "строительство многоэтажки",
+  ],
+  complaint: ["--Выберите раздел--", "жалоба на соседа", "жалоба на полицию"],
+  sign: ["--Выберите раздел--", "разрешение на установку знака"],
+  trade: [
+    "--Выберите раздел--",
+    "продажа пирожков",
+    "продажа цветов",
+    "продажа игрушек",
+    "продажа продуктов",
+  ],
+};
 
-  const formTypes = {
-    
-  }
+const serverFormS = {
+  construction: [
+    { text: "ФИО", type: "text" },
+    { text: "Адрес стройки", type: "text" },
+    { text: "Загрузите Ваш файл", type: "file" },
+  ],
+  complaint: [
+    { text: "ФИО", type: "text" },
+    { text: "Адрес правонарушения", type: "text" },
+    { text: "Загрузите фотодоказательство", type: "file" },
+  ],
+  sign: [
+    { text: "ФИО", type: "text" },
+    { text: "Адрес установки знака", type: "text" },
+  ],
+  trade: [
+    { text: "ФИО", type: "text" },
+    { text: "Адрес торгового объекта", type: "text" },
+    { text: "Загрузите согласования", type: "file" },
+  ],
+};
 
-console.log("starting server...")
+console.log("starting server...");
 const server = http.createServer(handler);
 
 // serverReq = {url: 'asdasd', method: 'GET'}
 // serverRes = {writer: function(){}}
 // handler(serverReq, serverRes)
 
-server.listen(8080)
+server.listen(8080);
 
 function handler(req, res) {
-    console.log("url", req.url)
-    console.log("method", req.method)
+  console.log("url", req.url);
+  console.log("method", req.method);
 
-    res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  });
 
-    if (req.url == '/construction') {
-        const response  = applicationTypes.construction
-        res.write(JSON.stringify(response))
-    } else if (req.url == '/complaint') {
-        const response  = applicationTypes.complaint
-        res.write(JSON.stringify(response))
-    } else if (req.url == '/sign') {
-        const response  = applicationTypes.sign
-        res.write(JSON.stringify(response))
-    } else if (req.url == '/trade') {
-        const response  = applicationTypes.trade
-        res.write(JSON.stringify(response))
-    } else {
-        const response  = {status: 'error', message: 'NOT FOUND'}
-        res.write(JSON.stringify(response))
-    }
-
-
-    res.end();
+  if (req.url == "/construction") {
+    const response = {
+      options: applicationTypes.construction,
+      form: serverFormS.construction,
+    };
+    res.write(JSON.stringify(response));
+  } else if (req.url == "/complaint") {
+    const response = {
+      options: applicationTypes.complaint,
+      form: serverFormS.complaint,
+    };
+    res.write(JSON.stringify(response));
+  } else if (req.url == "/sign") {
+    const response = {
+      options: applicationTypes.sign,
+      form: serverFormS.sign,
+    };
+    res.write(JSON.stringify(response));
+  } else if (req.url == "/trade") {
+    const response = {
+      options: applicationTypes.trade,
+      form: serverFormS.trade,
+    };
+    res.write(JSON.stringify(response));
+  } else {
+    const response = { status: "error", message: "NOT FOUND" };
+    res.write(JSON.stringify(response));
   }
+  
+  res.end();
+}
 
-  console.log("in the end")
+console.log(response);
+console.log("in the end");
