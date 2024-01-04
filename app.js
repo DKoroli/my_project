023c9a00c7ce967751c76ae44665f604d04ce321
DKoroli <114218001+DKoroli@users.noreply.github.com>
2024-01-04 13:18:54 +0200

    button for modal

--- app.js
+++ app.js
@@ -79,6 +79,18 @@ select.addEventListener("change", (e) => {
   });
 });
 
+// const button_for_modal = document.getElementById("button_for_modal");
+// button_for_modal.addEventListener("onclick", (e) => {
+//   const modal_window = document.getElementById("modal_window");
+//   modal_window.style.display = "block";
+// });
+
+const button_for_modal = document.getElementById("button_for_modal");
+button_for_modal.onclick = function () {
+  const modal_window = document.getElementById("modal_window");
+  modal_window.style.display = "block";
+};
+
 function addSelectForSubcat(arg) {
   const select = document.createElement("select");
   select.id = "addedSel";
